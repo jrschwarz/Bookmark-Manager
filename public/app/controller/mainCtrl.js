30020c01080db32fbae05d6a2f7e7a7ef5ddf4af
Joseph Schwarz <josephschwarz@Josephs-MacBook-Pro.local>
2016-06-17 13:25:52 -0400

    Beautified

--- public/app/controller/mainCtrl.js
+++ public/app/controller/mainCtrl.js
@@ -1,111 +1,105 @@
 angular.module('app').controller('mainCtrl', function($scope, $window, localStorageService) {
 
-	$scope.bookmarkInput = {
-		name: '',
-		url: '',
-		isFavorite: false,
-		editing: false
-	};
+  $scope.bookmarkInput = {
+    name: '',
+    url: '',
+    isFavorite: false,
+    editing: false
+  };
 
-	$scope.defaults = [ 
-		{ 
-			name:'Google',
-			url: 'http://google.com',
-		  	isFavorite: false,
-		  	editing: false
-		},
-		{
-			name: 'Facebook',
-			url: 'http://facebook.com',
-			isFavorite: false,
-			editing: false
-		},
-		{
-			name: 'Youtube',
-			url: 'http://youtube.com',
-			isFavorite: false,
-			editing: false
-		},
-		{
-			name: 'FAU Blackboard',
-			url: 'https://bb.fau.edu',
-			isFavorite: false,
-			editing: false
-		},
-		{
-			name: 'Apple',
-			url: 'http://apple.com',
-			isFavorite: false,
-			editing: false
-		}
-	];
+  $scope.defaults = [{
+    name: 'Google',
+    url: 'http://google.com',
+    isFavorite: false,
+    editing: false
+  }, {
+    name: 'Facebook',
+    url: 'http://facebook.com',
+    isFavorite: false,
+    editing: false
+  }, {
+    name: 'Youtube',
+    url: 'http://youtube.com',
+    isFavorite: false,
+    editing: false
+  }, {
+    name: 'FAU Blackboard',
+    url: 'https://bb.fau.edu',
+    isFavorite: false,
+    editing: false
+  }, {
+    name: 'Apple',
+    url: 'http://apple.com',
+    isFavorite: false,
+    editing: false
+  }];
 
-	var listInStore = localStorageService.get('bookmarks');
-	$scope.bookmarks = (listInStore != null) ? listInStore : $scope.defaults;
-	$scope.panelOpen = false;
+  var listInStore = localStorageService.get('bookmarks');
+  $scope.bookmarks = (listInStore !== null) ? listInStore : $scope.defaults;
+  $scope.panelOpen = false;
 
-	$scope.$watch('bookmarks', function() {
-		localStorageService.set('bookmarks', $scope.bookmarks);
-	}, true);
+  $scope.$watch('bookmarks', function() {
+    localStorageService.set('bookmarks', $scope.bookmarks);
+  }, true);
 
 
-	$scope.cancelEditing = function() {
-		$scope.bookmarks.forEach(function(b) {
-			b.editing = false;
-		})
-	}
+  $scope.cancelEditing = function() {
+    $scope.bookmarks.forEach(function(b) {
+      b.editing = false;
+    });
+  };
 
-	$scope.addBookmark = function() {
-		$scope.bookmarks.push($scope.bookmarkInput);
+  $scope.addBookmark = function() {
+    $scope.bookmarks.push($scope.bookmarkInput);
 
-		$scope.bookmarkInput = {
-			name: '',
-			url: '',
-			isFavorite: false,
-			editing: false
-		};
-	}
+    $scope.bookmarkInput = {
+      name: '',
+      url: '',
+      isFavorite: false,
+      editing: false
+    };
+  };
 
-	$scope.editBookmark = function(bookmark, e) {
+  $scope.editBookmark = function(bookmark, e) {
 
-		e.stopPropagation();
-		$scope.bookmarks.forEach(function(b) {
-			if(b == bookmark) b.editing = true;
-			else b.editing = false;
-		})
-	}
+    e.stopPropagation();
+    $scope.bookmarks.forEach(function(b) {
+      if (b == bookmark) b.editing = true;
+      else b.editing = false;
+    });
+  };
 
-	$scope.updateBookmark = function(bookmark) {
+  $scope.updateBookmark = function(bookmark) {
 
-		$scope.bookmarks.forEach(function(b) {
-			if(b == bookmark) b.editing = false;
-			else b.editing = false;
-		})
-	}
+    $scope.bookmarks.forEach(function(b) {
+      if (b == bookmark) b.editing = false;
+      else b.editing = false;
+    });
+  };
 
-	$scope.deleteBookmark = function(bookmark, e) {
+  $scope.deleteBookmark = function(bookmark, e) {
 
-		e.stopPropagation();
+    e.stopPropagation();
 
-		var index = $scope.bookmarks.indexOf(bookmark);
-		$scope.bookmarks.splice(index, 1);
-	}
+    var index = $scope.bookmarks.indexOf(bookmark);
+    $scope.bookmarks.splice(index, 1);
+  };
 
-	$scope.markFavorite = function(bookmark, e) {
+  $scope.markFavorite = function(bookmark, e) {
 
-		e.stopPropagation();
-		bookmark.isFavorite = true;
-	}
+    e.stopPropagation();
+    bookmark.isFavorite = true;
+  };
 
-	$scope.unmarkFavorite = function(bookmark, e) {
+  $scope.unmarkFavorite = function(bookmark, e) {
 
-		e.stopPropagation();
-		bookmark.isFavorite = false;
-	}
+    e.stopPropagation();
+    bookmark.isFavorite = false;
+  };
 
-	$scope.openWindow = function(bookmark, e) {
+  $scope.openWindow = function(bookmark, e) {
 
-		e.stopPropagation();
-		if (!bookmark.editing) $window.open(bookmark.url);
-	}
+    e.stopPropagation();
+    if (!bookmark.editing) $window.open(bookmark.url);
+  };
 });
